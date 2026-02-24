a059573d966a0f479ad1f001558d2968c8564f19
tromm <chldpwl5620@gmail.com>
2026-02-24 14:57:03 +0900

    fix: 배포 스크립트 output: 'standalone' 추가

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
   outputFileTracingRoot: path.join(__dirname),
   async rewrites() {
     return [
