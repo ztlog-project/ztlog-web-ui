dd5414564a9e612a198e4bcf2096568402d4687c
tromm <chldpwl5620@gmail.com>
2026-02-26 16:14:49 +0900

    fix: 백엔드 URL 및 포트 환경변수 설정

--- next.config.js
+++ next.config.js
@@ -9,7 +9,8 @@ const nextConfig = {
     return [
       {
         source: '/front/api/v1/:path*',
-        destination: 'http://localhost:8086/front/api/v1/:path*',
+        // destination: 'http://172.21.16.1:8086/front/api/v1/:path*',
+        destination: `${process.env.BACKEND_URL || 'http://localhost:8086'}/front/api/v1/:path*`,
       },
     ];
   },
